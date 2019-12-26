3f0eef353747412012a41a592cb0d5aeb4b72766
Peter Goa <grl0218@163.com>
2019-12-26 13:11:07 +0900

    debug

--- getColors.m
+++ getColors.m
@@ -4,6 +4,6 @@ numPerColor = ceil(NumColor^(1/3));
 r = (1:1:numPerColor)/numPerColor;
 g = (1:1:numPerColor)/numPerColor;
 b = (1:1:numPerColor)/numPerColor;
-for index = 1:1:numPerColor
+for index = 1:1:NumColor
     Colors(index,:) = [r(ceil(index/(numPerColor*numPerColor))),g(ceil((mod(index,numPerColor*numPerColor)+1)/numPerColor)),b(mod(mod(index,numPerColor*numPerColor),numPerColor)+1)];
 end
